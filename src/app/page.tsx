9c988da308e1c1f5100db009a4b472ba13cfdb30
fikriik <maldinurfikry@gmail.com>
2024-06-15 02:33:20 +0700

    update image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Navbar from "@/components/Navbar"
 import Ngerep from "@/components/Ngerep";
 import Seaction from "@/components/Seaction";
+import Sleder from "@/components/Sleder"
 
 export default function Home() {
   return (
     <div className="">
         <Navbar />
         <Ngerep />
+        <Sleder />
         <Seaction />
     </div>
   );
---
+++ src/components/Sleder.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const MarqueeWithImages = () => {
+  return (
+    <div className="bg-transparent border-t-2 border-b-2 border-[#f7f7f7] text-gray-800 p-4">
+      <div className="marquee-container">
+        <div className="marquee-images">
+          <img
+            src="/images/image1.png"
+            alt="Image 1"
+            className="w-30 h-14 transition-all duration-300 ease-in-out transform hover:scale-105 grayscale hover:grayscale-0"
+          />
+          <img
+            src="/images/image2.png"
+            alt="Image 2"
+            className="w-30 h-14 transition-all duration-200 ease-in-out transform hover:scale-105 grayscale hover:grayscale-0"
+          />
+          <img
+            src="/images/f.png"
+            alt="Image 3"
+            className="w-30 h-14 transition-all duration-300 ease-in-out transform hover:scale-105 grayscale hover:grayscale-0"
+          />
+          <img
+            src="/images/calculateurage.png"
+            alt="Image 4"
+            className="w-30 h-14 transition-all duration-300 ease-in-out transform hover:scale-105 grayscale hover:grayscale-0"
+          />
+          <img
+            src="/images/taqwim-logo.webp"
+            alt="Image 5"
+            className="w-30 h-14 transition-all duration-300 ease-in-out transform hover:scale-105 grayscale hover:grayscale-0"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MarqueeWithImages;
